b2e310b254bfcc13e4d707633b862484a44e5179
hellosamuel <samuelis419@gmail.com>
2019-12-15 16:03:06 +0900

    EDIT structure refactoring

--- src/containers/HighlightText/index.js
+++ src/containers/HighlightText/index.js
@@ -1,43 +1,57 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+import {Tooltip, Typography} from '@material-ui/core';
+import ColorHash from 'color-hash';
 
-import FilterPopover from '../../components/FilterPopover';
-import HighlightedText from './HighlightedText';
+import { preReg, afterReg } from '../../config'
 
-const HighlightText = ({ data, category }) => {
-  const [list, setList] = useState(data)
-  const [filterOpen, setFilterOpen] = useState(false)
-  const [location, setLocation] = useState({})
+const colorHash = new ColorHash()
 
-  const handleMouseUp = () => {
-    const selection =  getSelection()
-    if (selection.toString().trim()) {
-      const { right, top } = selection.getRangeAt(0).getClientRects()[0]
-      setFilterOpen(true)
-      setLocation({ left: right + 5, top })
-    }
-  }
+const useStyles = makeStyles(theme => ({
+  highlightText: {
+    padding: theme.spacing(0.25, 0.5),
+  },
+}));
 
-  const applyResult = result => {
-    setFilterOpen(false)
-  }
+const HighlightText = ({ data, ...handleEvent }) => {
+  const classes = useStyles()
 
   return (
     <>
-      {list.map(({ id, text }) => (
-        <HighlightedText
-          key={id}
-          text={text}
-          onMouseUp={handleMouseUp}
-        />
-      ))}
-      {filterOpen &&
-        <FilterPopover
-          open={filterOpen}
-          location={location}
-          category={category}
-          applyResult={applyResult}
-        />
-      }
+      {data.map(({ id, text }) => {
+        let offset = 0
+        const highlightedText = text.split(preReg).reduce((acc, val) => {
+          if (preReg.test(val)) {
+            const [origin, word, category] = [...val.matchAll(afterReg)][0]
+            acc.push(
+              <Tooltip key={offset} title={category}>
+                <Typography
+                  className={classes.highlightText}
+                  component="span"
+                  style={{ background: colorHash.hex(category) }}
+                >
+                  {word}
+                </Typography>
+              </Tooltip>
+            )
+            offset += origin.length
+          } else if (val) {
+            acc.push(
+              <Typography key={offset} component="span">
+                {val}
+              </Typography>
+            )
+            offset += val.length
+          }
+          return acc
+        }, [])
+
+        return (
+          <Typography key={id} gutterBottom {...handleEvent}>
+            {highlightedText}
+          </Typography>
+        )
+      })}
     </>
   )
 
--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles'
 import { Paper, Typography } from '@material-ui/core'
 
 import { data, category } from '../../config'
 
 import HighlightText from '../HighlightText'
+import FilterPopover from "../../components/FilterPopover";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,13 +15,40 @@ const useStyles = makeStyles(theme => ({
 
 const Main = ({ title }) => {
   const classes = useStyles()
+  const [list, setList] = useState(data)
+  const [filterOpen, setFilterOpen] = useState(false)
+  const [location, setLocation] = useState({})
+
+  const handleMouseUp = () => {
+    const selection =  getSelection()
+    if (selection.toString().trim()) {
+      const { right, top } = selection.getRangeAt(0).getClientRects()[0]
+      setFilterOpen(true)
+      setLocation({ left: right + 5, top })
+    }
+  }
+
+  const applyResult = result => {
+    setFilterOpen(false)
+  }
 
   return (
     <Paper className={classes.root}>
       <Typography variant="h3" align="center" gutterBottom>
         {title}
       </Typography>
-      <HighlightText data={data} category={category} />
+      <HighlightText
+        data={list}
+        onMouseUp={handleMouseUp}
+      />
+      {filterOpen &&
+        <FilterPopover
+          open={filterOpen}
+          location={location}
+          category={category}
+          applyResult={applyResult}
+        />
+      }
     </Paper>
   )
 }
